8b96e7a0a6fa8d7fdbc05b6436c53bc855beed3d
tt4g <mainlinett@gmail.com>
2020-09-13 09:28:31 +0900

    Provide Material-UI Theme

--- src/material-ui/MaterialUI.tsx
+++ src/material-ui/MaterialUI.tsx
@@ -1,11 +1,22 @@
 import { CssBaseline } from "@material-ui/core";
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@material-ui/core/styles";
 import React from "react";
 
+const theme = createMuiTheme();
+const responsiveTheme = responsiveFontSizes(theme, {
+  breakpoints: ["sm", "md", "lg"],
+  disableAlign: false,
+});
+
 const MaterialUI: React.FC = ({ children }) => (
-  <>
+  <ThemeProvider theme={responsiveTheme}>
     <CssBaseline />
     {children}
-  </>
+  </ThemeProvider>
 );
 
 export { MaterialUI };
